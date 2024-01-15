0da5781035dbca532840262a27e03cb4d14cd740
joetorsney <joetorsney@gmail.com>
2024-01-15 16:45:32 +0000

    Modify text on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,13 +14,13 @@ const timelineItems = [
     time: '2018 - 2023',
     title: 'The University of Sheffield',
     subtitle: '1st (hons) MComp Computer Science with a Year in Industry',
-    body: 'During my time at university, I gained strong foundational knowledge in theoretical computer science, algorithms, and logic. I also gained practical experience in software engineering, database engineering, cryptography, and machine learning. My Masters\' thesis investigated the impact of speech data on advanced machine learning models for singing voice conversion.',
+    body: 'I gained strong foundational knowledge in theoretical computer science, algorithms, and logic. I also gained practical experience in software engineering, database engineering, cryptography, and machine learning. My Masters\' thesis investigated the impact of speech data on advanced machine learning models for singing voice conversion.',
   },
   {
     time: 'Summer 2023',
     title: 'CERN Summer Student Program',
     subtitle: 'Full Stack Web Developer',
-    body: 'I joined the ALICE experiment as a Full Stack Web Developer, and delivered several features to the \'Bookkeeping\' web app. Bookkeeping is used to keep track of day-to-day operations at ALICE, such as the experiments\' configuration, LHC Fills, and data taking. The app also allows staff to communicate by writing \'Logs\'. I implemented feature where users could link Logs to LHC Fills, an entity in the database. This involved adding a new table to the database to represent the many-to-many relationship, adding API endpoints to support the creation of Logs with LHC Fills, displayed these on the front-end, and wrote integration tests. I am currently implementing a flexible data table component as an open source contributor. I published a report on my achievements during my placement which is available online the CERN document server.',
+    body: 'I joined the ALICE experiment as a Full Stack Web Developer, and delivered several features to the \'Bookkeeping\' web app used in the operation of the ALICE experiment. The app allows staff to communicate by writing \'Logs\'. I implemented feature where users could link Logs to a particular database entity. This involved adding a new table to the database to represent the many-to-many relationship, adding API endpoints to support the creation of Logs with the entity, displayed these on the front-end, and wrote integration tests. I am currently implementing a flexible data table component as an open source contributor. I published a report on my achievements during my placement which is available online the CERN document server.',
   },
   {
     time: '2020-2022',
@@ -32,7 +32,7 @@ const timelineItems = [
     time: '2020 - 2021',
     title: 'STFC',
     subtitle: 'Scientific Software Engineer',
-    body: 'During my Year in Industry as part of my degree, I worked as a scientific software engineer on the Mantid Project, which provides tools for data reduction and analysis of particle physics and materials science experiments. I was involved in all aspects of the software lifecycle, including sprint planning, development, unit/integration testing, code reviews, and gathering user feedback. I delivered several features to improve the user experience, including a feature where a tool that plots 3D data reacted to changes in the data elsewhere in the application. I did this by implementing an Observer pattern. I also fixed bugs related to concurrency and gained deep experience with python: the application interfaced with a C++ backend exposed to python, and extended matplotlib and numpy to bring custom functionality.',
+    body: 'As part of my degree I undertook an industrial placement on the Mantid Project, which provides tools for data reduction and analysis of particle physics and materials science experiments. I delivered several features to improve the user experience, in particular a feature requested by users where a tool that plots 3D data reacted to changes in the data elsewhere in the application. I did this by implementing an Observer pattern. I also fixed bugs related to concurrency and gained deep experience with python: The application interfaced with a C++ backend exposed to python, and I worked on extending matplotlib to bring custom functionality. I was involved in all aspects of the software lifecycle, including sprint planning, development, unit/integration testing, code reviews, and gathering user feedback.',
   }
 ]
 
